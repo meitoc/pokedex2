ae4b9e3a1e8849c0365342c0c4926cd266704625
Meitoc <info@meitoc.com>
2023-09-27 11:18:44 +0700

    pokedex

---
+++ src/app/config.js
@@ -0,0 +1,2 @@
+export const BASE_URL = process.env.REACT_APP_BACKEND_API; //'http://localhost:8000';//
+export const POKEMONS_PER_PAGE = 20;
---
+++ src/components/PageTitle.js
@@ -0,0 +1,23 @@
+import { Container, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import React from 'react'
+
+const styles = {
+    container: {
+        backgroundColor: "white",
+        backgroundImage: "url('/images/container_bg.png')"
+
+    }
+}
+
+
+export const PageTitle = () => {
+    return (
+        <Container maxWidth="lg" sx={styles.container}>
+            <Box container maxWidth="md" sx={{ margin: "auto!important", paddingX: "2rem", paddingY: "1rem", backgroundColor: "white" }}>
+                <Typography variant="h4" color="gray">Pokédex</Typography>
+                {/* <img src='International_Pokémon_logo.svg'/> */}
+            </Box>
+        </Container>
+    )
+}
